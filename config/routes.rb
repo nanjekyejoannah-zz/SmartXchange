be36b999b0c66b498219e791c780d39e42f22378
Sebastian Bogomir Peterlin <sebastianbogomirpeterlin@Sebastians-MacBook-Pro-3.local>
2016-06-10 12:21:15 +0200

    Fixed route for about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'about' => 'static_pages/about'
+  get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'login'   => 'sessions#new'
   get 'signup'  => 'user#new'
