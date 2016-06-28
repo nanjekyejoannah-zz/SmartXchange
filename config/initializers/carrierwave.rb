9132639bd782f85cfe39218f2b927d0eaf74a57a
Sebastian Bogomir Peterlin <sebastianbogomirpeterlin@Sebastians-MacBook-Pro-3.local>
2016-06-29 01:36:20 +0200

    Changed directory to correct bucket name, also changed region to match bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => 'AKIAJ42FOFV26EZ7NAKA',
     :aws_secret_access_key  => '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',
-    :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+    :region                 => 'us-standard'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'site_images'  # required
+  config.fog_directory  = 'smartxchange'  # required
   config.fog_public     = true     # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
