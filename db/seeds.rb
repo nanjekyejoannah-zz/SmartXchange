73bee5c621f496090178f235cd8f2de4fe914d6d
Sebastian Bogomir Peterlin <sebastianbogomirpeterlin@Sebastians-MacBook-Pro-3.local>
2016-07-01 23:44:30 +0200

    Changed header from Matches to Network, added images for seeded users, changed seed file to include 6 standard users (including me) and 5 random ones

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-u1 = User.create!(email: 'example@gmail.com',password: 'password', name: 'Sebastian Peterlin', language: 'Spanish', language_level: 6, title: "Co-founder of smartXchange, IMBA 2016 Candidate at IE Business School", image: File.open("app/assets/images/Sebastian_Peterlin professional.jpg"), age: 27);
+u = User.create!(email: 'example@gmail.com',password: 'password', name: 'Sebastian Peterlin', language: 'Spanish', language_level: 6, title: "Co-founder of smartXchange, IMBA 2016 Candidate at IE Business School", image: File.open("app/assets/images/Sebastian_Peterlin professional.jpg"), age: 27);
+u1 = User.create!(email: 'example1@gmail.com',password: 'password', name: 'Patsy Purdy', language: 'Spanish', language_level: 6, title: "English teacher, Masters in Communications graduate", image: File.open("app/assets/images/Patsy Purdy.jpg"), age: 26);
+u2 = User.create!(email: 'example2@gmail.com',password: 'password', name: 'Abigale Jacobson', language: 'French', language_level: 7, title: "Dentistry student at Complutense University of Madrid", image: File.open("app/assets/images/Abigale Jacobson.jpg"), age: 24);
+u3 = User.create!(email: 'example3@gmail.com',password: 'password', name: 'Coty Smitham', language: 'German', language_level: 5, title: "PhD in Mathematical Science at Complutense University of Madrid", image: File.open("app/assets/images/Coty Smitham.jpg"), age: 28);
+u4 = User.create!(email: 'example4@gmail.com',password: 'password', name: 'Janet Collier', language: 'French', language_level: 6, title: "Master in Psychology at Autonomous University of Madrid", image: File.open("app/assets/images/Janet Collier.jpg"), age: 29);
+u5 = User.create!(email: 'example5@gmail.com',password: 'password', name: 'Shanie Macejkovic', language: 'English', language_level: 5, title: "Business Development at Bla Bla Car Madrid", image: File.open("app/assets/images/Shanie Macejkovic.jpg"), age: 26);
 
-languages = ["Spanish","English","German"]
+languages = ["Spanish","English","German","French"]
 language_levels = [1,2,3,4,5,6,7,8,9,10]
 ages = [23,25,27,29,31,33,35,37]
 titles = ["Monetizing intuitive partnerships", "Deploying vertical web services", "Generating frictionless technologies", "Innovating world class niches", "Syndicate b2c niches", "Harnessing front-end metrics", "Generating b2c bandwidth", "Optimizing holistic systems"]
 
 
 
-10.times do |n|
+
+
+5.times do |n|
   name  = Faker::Name.name
-  email = "example#{n+1}@gmail.com"
+  email = "example#{n+6}@gmail.com"
   password = "password"
   age = ages[rand(ages.count)]
   language = languages[rand(languages.count)]
