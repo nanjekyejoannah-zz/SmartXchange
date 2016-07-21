79697fa3e1ed61cce0ef5e08bea60ef82055328f
Sebastian Bogomir Peterlin <sebastianbogomirpeterlin@Sebastians-MacBook-Pro-3.local>
2016-07-21 02:16:07 +0200

    Changed flag icons according to IconArchive, looks better now

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -9,15 +9,15 @@ module MessagesHelper
     filtered = (1 - message.sender.language_level.to_f / 6)
     filtered = number_to_percentage(filtered*100)
     if message.sender.language == "Spanish"
-      image_tag('spain-flag-circular.gif', :style => "-webkit-filter: grayscale(#{filtered});", alt: 'Spanish')
+      image_tag('spain-flag-circular.png', :style => "-webkit-filter: grayscale(#{filtered});", alt: 'Spanish')
     elsif message.sender.language == "Italian"
-      image_tag('italy-flag-circular.gif', :style => "-webkit-filter: grayscale(#{filtered});", alt: 'Italian')
+      image_tag('italy-flag-circular.png', :style => "-webkit-filter: grayscale(#{filtered});", alt: 'Italian')
     elsif message.sender.language == "German"
-      image_tag('germany-flag-circular.gif', :style => "-webkit-filter: grayscale(#{filtered});", alt: 'German')
+      image_tag('germany-flag-circular.png', :style => "-webkit-filter: grayscale(#{filtered});", alt: 'German')
     elsif message.sender.language == "English"
       image_tag('united-kingdom-flag-circular.png', :style => "-webkit-filter: grayscale(#{filtered});", alt: 'English')
     elsif message.sender.language == "French"
-      image_tag('france-flag-circular.gif', :style => "-webkit-filter: grayscale(#{filtered});", alt: 'French')
+      image_tag('france-flag-circular.png', :style => "-webkit-filter: grayscale(#{filtered});", alt: 'French')
     end
   end
 
