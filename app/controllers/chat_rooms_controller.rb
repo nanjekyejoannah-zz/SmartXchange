e0055963bcbd1f7de68f4e3afe0d01f93ba11e8c
Sebastian Peterlin <speterlin@gmail.com>
2016-08-20 01:29:46 +0200

    Got rid of messages: :sender in includes in show method because this wasn't speeding anytning up, sender is still called for each message regardless.

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -19,7 +19,7 @@ class ChatRoomsController < ApplicationController
   end
 
   def show
-    @chat_room ||= ChatRoom.includes(:messages, {messages: :sender}).find_by(id: params[:id])
+    @chat_room ||= ChatRoom.includes(:messages).find_by(id: params[:id])
     @message = Message.new
     @receiver = chat_room_interlocutor(@chat_room, current_user)
     # updating notifications for user as they visit chat room
