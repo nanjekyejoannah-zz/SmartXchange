fee7ffb6454c62c80e39895ed13da65bed285451
Sebastian Peterlin <speterlin@gmail.com>
2016-09-15 00:08:57 +0200

    Change code so that every time post is added it is moved up according to its value and updated_at and also make sure sum on empty column returns 0 so that code runs correctly Boards_controller.rb: add coalesce to find_by_sql method since postgres wasn’t recognizing sum on empty column as 0. Posts / create.js.erb: add code for moving post up based on popularity (since it starts with 0 up votes), add conditional in while loop before assigning true value to moved variable. Posts / downvote.js.erb & upvote.js.erb: add conditional in while loop before assigning true value to moved variable. Posts / followers.js.erb: add dots before and after Hide button to increase spacing and make it more clickable on mobile device.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,11 +6,12 @@ class BoardsController < ApplicationController
     @board = Board.first
     # refactor sql query, right now orders by sum(value) then updated_at, also assuming all posts are associated with the first board, and all comments are for post
     # @posts = Post.includes().joins(:votes).select('votable_id, count(votable_id) as votes_count, sum(value) as votes_value_sum').group(:votable_id).order('sum(value) desc')
+    # coalesce because postgres does not return sum of empty column
     @posts = Post.find_by_sql("
       select *
       from posts
       left join (
-        select votable_id, count(votable_id) as votes_count, sum(value) as votes_value_sum
+        select votable_id, count(votable_id) as votes_count, coalesce(sum(value),0) as votes_value_sum
         from votes
         group by votable_id
       ) as sums on
