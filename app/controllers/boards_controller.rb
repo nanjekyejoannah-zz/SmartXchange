fec13947879e35ddc697502c64adcafe365bf312
Sebastian Peterlin <speterlin@gmail.com>
2016-09-18 13:37:11 +0200

    Alter sql query to try to make it work on postgres and to simplify it

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,15 +8,11 @@ class BoardsController < ApplicationController
     # @posts = Post.includes().joins(:votes).select('votable_id, count(votable_id) as votes_count, sum(value) as votes_value_sum').group(:votable_id).order('sum(value) desc')
     # coalesce because postgres does not return sum of empty column
     @posts = Post.find_by_sql("
-      select *
-      from posts
-      left join (
-        select votable_id, count(votable_id) as votes_count, coalesce(sum(value),0) as votes_value_sum
-        from votes
-        group by votable_id
-      ) as sums on
-      posts.id = sums.votable_id
-      order by sums.votes_value_sum, posts.updated_at").reverse
+      select p.*, v.votable_id, count(v.votable_id) as votes_count, coalesce(sum(v.value),0) as votes_value_sum
+      from posts p
+      left join votes v on p.id = v.votable_id
+      group by v.votable_id
+      order by votes_value_sum desc, p.updated_at desc")
       # only way to get includes to work on the array returned from the sql statement above, maybe refactor don't need all followers information
       ActiveRecord::Associations::Preloader.new.preload(@posts, [:owner, :comments, {comments: :owner}, :followers])
     if user_count_unread_posts(current_user) > 0
