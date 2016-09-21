42d026d87122898da8cc1ffbd571daa80d14296d
Sebastian Peterlin <speterlin@gmail.com>
2016-09-22 01:02:57 +0200

    Add if Rails.env.development? to loading of environment variables from application.yml file since this file doesn't exist in production. Will set heroku config variables manually

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,12 @@ require 'omniauth'
 Bundler.require(*Rails.groups)
 
 # preload tokens in application.yml to local ENV
-config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-config.merge! config.fetch(Rails.env, {})
-config.each do |key, value|
-  ENV[key] = value.to_s unless value.kind_of? Hash
+if Rails.env.development?
+  config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+  config.merge! config.fetch(Rails.env, {})
+  config.each do |key, value|
+    ENV[key] = value.to_s unless value.kind_of? Hash
+  end
 end
 
 module SmartXchange
